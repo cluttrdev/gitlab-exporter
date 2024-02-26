4f91e982d1ce9d81d3409eb9ef54cf097800da7b
cluttrdev <andreas@cluttr.tech>
2024-02-26 23:09:15 +0100

    Start adding tests

---
+++ test/grpc/client_test.go
@@ -0,0 +1,85 @@
+package grpc_test
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"testing"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/test/bufconn"
+
+	grpc_client "github.com/cluttrdev/gitlab-exporter/grpc/client"
+	"github.com/cluttrdev/gitlab-exporter/protobuf/typespb"
+
+	grpc_mock "github.com/cluttrdev/gitlab-exporter/test/mock/grpc"
+)
+
+const bufSize int = 1024 * 1024
+
+func newServerAndClient() (*grpc_mock.MockExporterServer, *grpc_client.Client, error) {
+	server := grpc_mock.NewMockExporterServer()
+
+	listener := bufconn.Listen(bufSize)
+	go func() {
+		if err := server.Serve(listener); err != nil {
+			fmt.Fprint(os.Stderr, err)
+		}
+	}()
+
+	client, err := grpc_client.NewCLient(
+		context.Background(),
+		"bufnet",
+		grpc.WithContextDialer(func(context.Context, string) (net.Conn, error) {
+			return listener.Dial()
+		}),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+
+	return server, client, err
+}
+
+func Test_RecordPipelines(t *testing.T) {
+	server, client, err := newServerAndClient()
+	defer server.GracefulStop()
+	if err != nil {
+		t.Error(err)
+	}
+
+	data := []*typespb.Pipeline{
+		{
+			Id: 42,
+		},
+	}
+
+	server.ExpectPipelines(data)
+
+	ctx := context.Background()
+	if err := grpc_client.RecordPipelines(client, ctx, data); err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_RecordJobs(t *testing.T) {
+	server, client, err := newServerAndClient()
+	defer server.GracefulStop()
+	if err != nil {
+		t.Error(err)
+	}
+
+	data := []*typespb.Job{
+		{
+			Id:       42,
+			Pipeline: nil,
+		},
+	}
+
+	server.ExpectJobs(data)
+
+	ctx := context.Background()
+	if err := grpc_client.RecordJobs(client, ctx, data); err != nil {
+		t.Error(err)
+	}
+}
---
+++ test/mock/grpc/server.go
@@ -0,0 +1,79 @@
+package grpc_mock
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net"
+
+	"github.com/google/go-cmp/cmp"
+	"google.golang.org/grpc"
+
+	"github.com/cluttrdev/gitlab-exporter/protobuf/servicepb"
+	"github.com/cluttrdev/gitlab-exporter/protobuf/typespb"
+)
+
+type MockExporterServer struct {
+	servicepb.UnimplementedGitLabExporterServer
+	server *grpc.Server
+
+	expectedPipelines []*typespb.Pipeline
+	expectedJobs      []*typespb.Job
+}
+
+func NewMockExporterServer() *MockExporterServer {
+	return &MockExporterServer{
+		server: grpc.NewServer(),
+	}
+}
+
+func (ms *MockExporterServer) Serve(lis net.Listener) error {
+	servicepb.RegisterGitLabExporterServer(ms.server, ms)
+
+	return ms.server.Serve(lis)
+}
+
+func (ms *MockExporterServer) GracefulStop() {
+	ms.server.GracefulStop()
+}
+
+func (ms *MockExporterServer) Stop() {
+	ms.server.Stop()
+}
+
+func (ms *MockExporterServer) ExpectPipelines(data []*typespb.Pipeline) {
+	ms.expectedPipelines = data
+}
+
+func (ms *MockExporterServer) ExpectJobs(data []*typespb.Job) {
+	ms.expectedJobs = data
+}
+
+func check[T any](srv *MockExporterServer, want []*T, got []*T, opts ...cmp.Option) (int32, error) {
+	var errs error
+
+	var recorded int32 = 0
+	for i, got := range got {
+		a, _ := json.Marshal(want[i])
+		b, _ := json.Marshal(got)
+
+		if diff := cmp.Diff(a, b, opts...); diff != "" {
+			errs = errors.Join(errs, fmt.Errorf("Mismatch (-want, +got):\n%s", diff))
+		} else {
+			recorded++
+		}
+	}
+
+	return recorded, errs
+}
+
+func (ms *MockExporterServer) RecordPipelines(ctx context.Context, r *servicepb.RecordPipelinesRequest) (*servicepb.RecordSummary, error) {
+	n, err := check(ms, ms.expectedPipelines, r.Data)
+	return &servicepb.RecordSummary{RecordedCount: n}, err
+}
+
+func (ms *MockExporterServer) RecordJobs(ctx context.Context, r *servicepb.RecordJobsRequest) (*servicepb.RecordSummary, error) {
+	n, err := check(ms, ms.expectedJobs, r.Data)
+	return &servicepb.RecordSummary{RecordedCount: n}, err
+}
