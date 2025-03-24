ed49d92a4edfc4626ac3a8b710a81e62b5e210e4
Andreas Kunze <andreas@cluttr.dev>
2025-03-24 17:23:01 +0100

    feat: Add job log property parser

--- internal/gitlab/rest/job_logs.go
+++ internal/gitlab/rest/job_logs.go
@@ -5,6 +5,8 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"io"
+	"unicode/utf8"
 
 	_gitlab "gitlab.com/gitlab-org/api/client-go"
 
@@ -12,8 +14,9 @@ import (
 )
 
 type JobLogData struct {
-	Sections []SectionData `json:"sections"`
-	Metrics  []MetricData  `json:"metrics"`
+	Sections   []SectionData  `json:"sections"`
+	Metrics    []MetricData   `json:"metrics"`
+	Properties []PropertyData `json:"properties"`
 }
 
 type MetricData struct {
@@ -21,6 +24,11 @@ type MetricData struct {
 	Labels    map[string]string `json:"labels"`
 	Value     float64           `json:"value"`
 	Timestamp int64             `json:"timestamp"`
+}
+
+type PropertyData struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
 }
 
 func (c *Client) GetJobLogData(ctx context.Context, projectId int64, jobId int64) (JobLogData, error) {
@@ -39,31 +47,46 @@ func (c *Client) GetJobLogData(ctx context.Context, projectId int64, jobId int64
 
 func ParseJobLog(trace *bytes.Reader) (JobLogData, error) {
 	var (
-		data     JobLogData
-		sections sectionStack
-		parser   expfmt.TextParser
+		data           JobLogData
+		sections       sectionStack
+		parser         expfmt.TextParser
+		propertyParser jobLogPropertyParser
 	)
 
 	const (
-		METRIC_MARKER  = `METRIC_`
-		SECTION_MARKER = `section_`
+		METRIC_MARKER   = `METRIC_`
+		PROPERTY_MARKER = `PROPERTY_`
+		SECTION_MARKER  = `section_`
 	)
 
 	scanner := bufio.NewScanner(trace)
 	for scanner.Scan() {
 		line := scanner.Bytes()
+
 		if bytes.HasPrefix(line, []byte(METRIC_MARKER)) {
-			metric, err := parser.LineToMetric(line[7:])
-			if err != nil {
-				// TODO: what?
+			metric, err := parser.LineToMetric(line[len(METRIC_MARKER):])
+			if err != nil || metric == nil {
+				// we ignore parsing errors here
+				// TODO: should we handle them somehow?
 				continue
 			}
+
 			data.Metrics = append(data.Metrics, MetricData{
 				Name:      metric.Name,
 				Labels:    convertMetricLabels(metric.Labels),
 				Value:     metric.Value,
 				Timestamp: metric.TimestampMs,
 			})
+		}
+		if bytes.HasPrefix(line, []byte(PROPERTY_MARKER)) {
+			property, err := propertyParser.LineToProperty(line[len(PROPERTY_MARKER):])
+			if err != nil || property == nil {
+				// we ignore parsing errors here
+				// TODO: should we handle them somehow?
+				continue
+			}
+
+			data.Properties = append(data.Properties, *property)
 		}
 
 		var i, j int
@@ -105,4 +128,150 @@ func convertMetricLabels(pairs []expfmt.MetricLabelPair) map[string]string {
 		labels[p.Name] = p.Value
 	}
 	return labels
+}
+
+type jobLogPropertyParser struct {
+	buf *bufio.Reader
+
+	currentByte  byte
+	currentToken bytes.Buffer
+	err          error
+
+	labelPair PropertyData
+}
+
+func (p *jobLogPropertyParser) LineToProperty(line []byte) (*PropertyData, error) {
+	if len(line) == 0 {
+		return nil, nil
+	}
+	p.reset(bytes.NewReader(line))
+
+	return p.lineToProperty()
+}
+
+func (p *jobLogPropertyParser) reset(in io.Reader) {
+	p.labelPair = PropertyData{}
+	if p.buf == nil {
+		p.buf = bufio.NewReader(in)
+	} else {
+		p.buf.Reset(in)
+	}
+	p.err = nil
+}
+
+func (p *jobLogPropertyParser) lineToProperty() (*PropertyData, error) {
+	p.currentByte, p.err = p.buf.ReadByte()
+	if p.err != nil {
+		return nil, p.err
+	}
+
+	// parse name
+	if p.readTokenAsLabelName(); p.err != nil {
+		return nil, p.err
+	}
+	if p.currentToken.Len() == 0 {
+		return nil, fmt.Errorf("empty label name")
+	}
+	p.labelPair.Name = p.currentToken.String()
+
+	// check for '='
+	if p.skipBlankTabIfCurrentBlankTab(); p.err != nil {
+		return nil, p.err
+	}
+	if p.currentByte != '=' {
+		return nil, fmt.Errorf("expected '=' after label name, found %q", p.currentByte)
+	}
+
+	// parse value
+	if p.skipBlankTab(); p.err != nil {
+		return nil, p.err
+	}
+	if p.currentByte != '"' {
+		return nil, fmt.Errorf("expected '\"' at start of label value, found %q", p.currentByte)
+	}
+	if p.readTokenAsLabelValue(); p.err != nil {
+		return nil, p.err
+	}
+
+	value := p.currentToken.String()
+	if !utf8.ValidString(value) {
+		return nil, fmt.Errorf("invalid label value %q", value)
+	}
+	p.labelPair.Value = value
+
+	return &p.labelPair, nil
+}
+
+func (p *jobLogPropertyParser) readTokenAsLabelName() {
+	p.currentToken.Reset()
+	if !isValidLabelNameStart(p.currentByte) {
+		return
+	}
+	for {
+		p.currentToken.WriteByte(p.currentByte)
+		p.currentByte, p.err = p.buf.ReadByte()
+		if p.err != nil || !isValidLabelNameContinuation(p.currentByte) {
+			return
+		}
+	}
+}
+
+func (p *jobLogPropertyParser) readTokenAsLabelValue() {
+	p.currentToken.Reset()
+	escaped := false
+	for {
+		if p.currentByte, p.err = p.buf.ReadByte(); p.err != nil {
+			return
+		}
+		if escaped {
+			switch p.currentByte {
+			case '"', '\\':
+				p.currentToken.WriteByte(p.currentByte)
+			case 'n':
+				p.currentToken.WriteByte('\n')
+			default:
+				p.err = fmt.Errorf("invalid escape sequence '\\%c'", p.currentByte)
+				return
+			}
+			escaped = false
+			continue
+		}
+		switch p.currentByte {
+		case '"':
+			return
+		case '\n':
+			p.err = fmt.Errorf("label value %q contains unescaped new-line", p.currentToken.String())
+			return
+		case '\\':
+			escaped = true
+		default:
+			p.currentToken.WriteByte(p.currentByte)
+		}
+	}
+}
+
+func (p *jobLogPropertyParser) skipBlankTabIfCurrentBlankTab() {
+	if isBlankOrTab(p.currentByte) {
+		p.skipBlankTab()
+	}
+}
+
+func (p *jobLogPropertyParser) skipBlankTab() {
+	for {
+		if p.currentByte, p.err = p.buf.ReadByte(); p.err != nil || !isBlankOrTab(p.currentByte) {
+			return
+		}
+	}
+}
+
+func isValidLabelNameStart(b byte) bool {
+	return (b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z') || b == '_'
+}
+
+func isValidLabelNameContinuation(b byte) bool {
+	return isValidLabelNameStart(b) || (b >= '0' && b <= '9')
+}
+
+func isBlankOrTab(b byte) bool {
+	return b == ' ' || b == '\t'
 }
