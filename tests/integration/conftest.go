5a31d77ea8c96253593dc7999ba8d9d265be3556
Andreas Kunze <andreas@cluttr.dev>
2025-10-24 16:16:20 +0200

    test(integration): Use latest clickhouse-server

--- tests/integration/conftest.go
+++ tests/integration/conftest.go
@@ -38,7 +38,7 @@ import (
 var testUUID = uuid.NewString()[0:12]
 var testTimestamp = time.Now().UnixMilli()
 
-const defaultClickHouseVersion = "24.8"
+const defaultClickHouseVersion = "latest"
 
 func GetClickHouseTestVersion() string {
 	return GetEnv("CLICKHOUSE_VERSION", defaultClickHouseVersion)
