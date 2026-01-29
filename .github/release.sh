0a253263048114791d32f87c79871525156b62fc
Andreas Kunze <andreas@cluttr.dev>
2026-01-29 09:21:30 +0100

    ci: Fix github release script

--- .github/release.sh
+++ .github/release.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 echo "Fetching GitLab release"
-project_id=50817395 # akun73/gitlab-exporter
+project_id=50817395 # gitlab.com/gitlab-exporter/gitlab-exporter
 response=$(
     curl -sSL --fail-with-body \
         "https://gitlab.com/api/v4/projects/${project_id}/releases/${TAG_NAME}"
@@ -14,13 +14,6 @@ fi
 
 # extract release notes
 release_notes=$(jq -r '.description' <<< "${response}" | sed 's/$/\\n/' | tr -d '\n')
-
-# adjust commit link url
-release_notes=$(sed 's#gitlab.com/akun73/gitlab-exporter/-/commit/#github.com/cluttrdev/gitlab-exporter/commit/#g' <<< "${release_notes}")
-
-# prepend link to release assets
-release_assets_url="https://gitlab.com/akun73/gitlab-exporter/-/releases/${TAG_NAME}"
-release_notes=$(printf '## Release Assets\\n\\nPlease find the release assets [here](%s).\\n\\n%s' "${release_assets_url}" "$release_notes")
 
 echo "Creating GitHub release"
 response=$(
